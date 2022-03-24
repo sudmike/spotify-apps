c78aa2d3f2a05f28d654dc9f1c38ba2e1aff24d2
sudmike <mike.sudhaus@gmail.com>
2022-03-24 15:16:37 +0100

    Added environmental variable files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MergerModule } from './merger/merger.module';
+import { ConfigModule } from '@nestjs/config';
+
+const ENV = process.env.NODE_ENV;
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: !ENV ? '.env' : `.env.${ENV}`,
+      isGlobal: true,
+    }),
+    MergerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
