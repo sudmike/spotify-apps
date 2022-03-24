d449be746682848b28967411bd970677c01dc0a3
sudmike <mike.sudhaus@gmail.com>
2022-03-24 15:26:01 +0100

    Added initial authorization for Spotify with OAuth 2.0

---
+++ backend/src/services/spotify.service.ts
@@ -0,0 +1,82 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import SpotifyWebApi from 'spotify-web-api-node';
+
+@Injectable()
+export class SpotifyService {
+  private spotifyApi = new SpotifyWebApi();
+
+  // contains state codes connected to requests that were sent out to Spotify
+  private states: { state: string; clientId: string }[] = [];
+
+  /**
+   * Starts off the OAuth flow by creating and returning URL for OAuth redirection.
+   * @param credentials Spotify application credentials.
+   * @param scopes The scopes that are requested from Spotify.
+   */
+  loginRedirect(credentials: SpotifyCredentials, scopes: string[]): string {
+    this.spotifyApi.setCredentials(credentials);
+
+    const state = generateRandomString(32);
+    this.states.push({ state, clientId: credentials.clientId });
+
+    return this.spotifyApi.createAuthorizeURL(scopes, state);
+  }
+
+  /**
+   * Completes the OAuth flow by sending a post request to Spotify for the tokens.
+   * @param credentials Spotify application credentials.
+   * @param state The state that was sent with the initial request. Protection from CSRF.
+   * @param code The authorization code provided by Spotify.
+   */
+  async callback(credentials: SpotifyCredentials, state: string, code: string) {
+    this.spotifyApi.setCredentials(credentials);
+
+    // check state to prevent CSRF
+    const index = this.states.findIndex(
+      (tuple) =>
+        tuple.state === state && tuple.clientId === credentials.clientId,
+    );
+
+    if (index > -1) {
+      // remove entry from states
+      this.states.splice(index);
+
+      // request tokens from Spotify
+      try {
+        return { ...(await this.spotifyApi.authorizationCodeGrant(code)).body };
+      } catch (e) {
+        throw new UnauthorizedException(
+          undefined,
+          `Failed to finish Spotify OAuth flow. Error: ${e.body.error}, error description: ${e.body.error_description}.`,
+        );
+      }
+    } else {
+      // send an error if the state is invalid
+      throw new UnauthorizedException(
+        undefined,
+        'Spotify callback not allowed, due to invalid state.',
+      );
+    }
+  }
+}
+
+/**
+ * Function that generates random string for OAuth states.
+ * @param length The length of the string that is generated.
+ */
+function generateRandomString(length: number): string {
+  let text = '';
+  const possible =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+export type SpotifyCredentials = {
+  clientId: string;
+  clientSecret?: string;
+  redirectUri: string;
+};
