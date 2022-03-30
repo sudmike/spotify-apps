19f22917b39750f04b99a91d577fa3bf29dc537d
sudmike <mike.sudhaus@gmail.com>
2022-03-30 18:20:36 +0200

    Added general database interaction

--- backend/src/merger/merger.module.ts
+++ backend/src/merger/merger.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MergerController } from './merger.controller';
 import { SpotifyService } from '../services/spotify.service';
+import { FirestoreService } from '../services/external/firestore.service';
+import { IDatabaseService } from '../services/IDatabase.service';
 
 @Module({
   controllers: [MergerController],
-  providers: [SpotifyService],
+  providers: [
+    SpotifyService,
+    { provide: IDatabaseService, useClass: FirestoreService },
+  ],
 })
 export class MergerModule {}
