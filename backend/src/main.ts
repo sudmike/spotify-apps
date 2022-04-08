c2293ee4a5c1ba174c7fc8a8a17047a4b2cedc48
sudmike <mike.sudhaus@gmail.com>
2022-04-08 23:35:32 +0200

    Added OpenAPI generation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Spotify apps NestJS backend')
+    .setDescription('The API for the Spotify apps')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(
     new ValidationPipe({
       forbidUnknownValues: true,
