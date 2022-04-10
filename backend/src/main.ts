8f78cbcc2ec4667a8c9d15397588ec48cbe35dd9
sudmike <mike.sudhaus@gmail.com>
2022-04-10 18:46:19 +0200

    Added client API generation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -21,6 +22,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  fs.writeFileSync('./openapi/openapi.json', JSON.stringify(document));
   await app.listen(3000);
 }
 bootstrap();
