7d21094225271d3ad0a6e5b3423e952ea433bf10
sudmike <mike.sudhaus@gmail.com>
2022-04-26 16:43:46 +0200

    Added guard to use for protected components

---
+++ frontends/merger/src/app/_guards/auth.guard.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {
+  Router,
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { ApiService } from '../services/api.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router, private api: ApiService) {}
+
+  async canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const loaded = sessionStorage.getItem('loaded');
+    const id = localStorage.getItem('id');
+    console.log('loaded', loaded);
+
+    if (!id) {
+      await this.router.navigate(['/login'], {
+        queryParams: { returnUrl: state.url },
+      });
+      return false;
+    } else {
+      if (loaded) return true;
+      else {
+        // check log in
+        if (await this.api.checkAuth()) {
+          sessionStorage.setItem('loaded', 'true');
+          return true;
+        } else return false;
+      }
+    }
+  }
+}
