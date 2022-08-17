0c5f6b42e90236355f1e27e5bf4570548e309c53
sudmike <mike.sudhaus@gmail.com>
2022-08-17 12:32:32 +0200

    Added theme

--- frontends/merger/src/openapi/base.ts
+++ frontends/merger/src/openapi/base.ts
@@ -13,10 +13,10 @@
  */
 
 
-import { Configuration } from "./configuration";
+import {Configuration} from "./configuration";
 // Some imports not used depending on template conditions
 // @ts-ignore
-import globalAxios, { AxiosPromise, AxiosInstance, AxiosRequestConfig } from 'axios';
+import globalAxios, {AxiosInstance, AxiosPromise, AxiosRequestConfig} from 'axios';
 
 export const BASE_PATH = "http://localhost".replace(/\/+$/, "");
 
@@ -25,10 +25,10 @@ export const BASE_PATH = "http://localhost".replace(/\/+$/, "");
  * @export
  */
 export const COLLECTION_FORMATS = {
-    csv: ",",
-    ssv: " ",
-    tsv: "\t",
-    pipes: "|",
+  csv: ",",
+  ssv: " ",
+  tsv: "\t",
+  pipes: "|",
 };
 
 /**
@@ -37,8 +37,8 @@ export const COLLECTION_FORMATS = {
  * @interface RequestArgs
  */
 export interface RequestArgs {
-    url: string;
-    options: AxiosRequestConfig;
+  url: string;
+  options: AxiosRequestConfig;
 }
 
 /**
@@ -47,15 +47,15 @@ export interface RequestArgs {
  * @class BaseAPI
  */
 export class BaseAPI {
-    protected configuration: Configuration | undefined;
+  protected configuration: Configuration | undefined;
 
-    constructor(configuration?: Configuration, protected basePath: string = BASE_PATH, protected axios: AxiosInstance = globalAxios) {
-        if (configuration) {
-            this.configuration = configuration;
-            this.basePath = configuration.basePath || this.basePath;
-        }
+  constructor(configuration?: Configuration, protected basePath: string = BASE_PATH, protected axios: AxiosInstance = globalAxios) {
+    if (configuration) {
+      this.configuration = configuration;
+      this.basePath = configuration.basePath || this.basePath;
     }
-};
+  }
+}
 
 /**
  *
@@ -65,7 +65,8 @@ export class BaseAPI {
  */
 export class RequiredError extends Error {
   override name: "RequiredError" = "RequiredError";
-    constructor(public field: string, msg?: string) {
-        super(msg);
-    }
+
+  constructor(public field: string, msg?: string) {
+    super(msg);
+  }
 }
