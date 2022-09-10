5537a13b1bfb4a7b6b593c3b3a7bf96a7ad446fb
sudmike <mike.sudhaus@gmail.com>
2022-09-10 13:27:23 +0200

    Solved bug that navbar does not refresh correctly

--- frontends/merger/src/app/navbar/navbar.component.ts
+++ frontends/merger/src/app/navbar/navbar.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.less'],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, DoCheck {
   show = false;
 
   ngOnInit(): void {
+    this.show = window.location.pathname !== '/';
+  }
+
+  ngDoCheck(): void {
+    // necessary for when the route changes
     this.show = window.location.pathname !== '/';
   }
 }
