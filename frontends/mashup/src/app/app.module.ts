1dbea312e5daa5bf3d79f9729de8d021f0c031ec
sudmike <mike.sudhaus@gmail.com>
2023-05-17 16:34:21 +0200

    Added Learn More page

--- frontends/mashup/src/app/app.module.ts
+++ frontends/mashup/src/app/app.module.ts
@@ -35,6 +35,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatMenuModule } from '@angular/material/menu';
+import { LearnComponent } from './learn/learn.component';
 
 @NgModule({
   declarations: [
@@ -50,6 +51,7 @@ import { MatMenuModule } from '@angular/material/menu';
     ConfigPaneComponent,
     NavbarComponent,
     SongNumberPaneComponent,
+    LearnComponent,
   ],
   imports: [
     BrowserModule,
