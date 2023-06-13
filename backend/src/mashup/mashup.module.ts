dcd2bb2bee8babfc5b2978530999da378a58bd93
sudmike <mike.sudhaus@gmail.com>
2023-06-13 14:06:14 +0200

    Added logging service for Google Cloud Logging

--- backend/src/mashup/mashup.module.ts
+++ backend/src/mashup/mashup.module.ts
@@ -3,9 +3,10 @@ import { MashupController } from './mashup.controller';
 import { DatabaseService } from './services/database.service';
 import { SpotifyService } from './services/spotify.service';
 import { BatchService } from './services/batch.service';
+import { LoggingService } from './services/logging.service';
 
 @Module({
   controllers: [MashupController],
-  providers: [SpotifyService, DatabaseService, BatchService],
+  providers: [SpotifyService, DatabaseService, BatchService, LoggingService],
 })
 export class MashupModule {}
